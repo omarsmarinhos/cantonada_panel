e6808efb72ef107ec383bd63059e053c0d56f8fb
omarsmarinhos <omarmarinosaguilar@gmail.com>
2024-12-07 10:32:04 -0500

    edit customers

--- src/app/domains/customers/customers.component.ts
+++ src/app/domains/customers/customers.component.ts
@@ -40,7 +40,7 @@ export default class CustomersComponent {
   dataSourceCustomers = signal<MatTableDataSource<Customer>>(
     new MatTableDataSource<Customer>()
   );
-  displayedColumns: string[] = ['tNombre', 'tApellido', 'tTelefono', 'tEmail', 'fFechaRegistro', 'fUltimoAcceso', 'tEstado'];
+  displayedColumns: string[] = ['tNombreCompleto', 'tTelefono', 'tEmail', 'lGoogle', 'fFechaRegistro', 'fUltimoAcceso', 'tEstado'];
 
   totalItems = signal(0);
   selectedState: string = '';
